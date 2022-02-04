52b975cb149f8126d7e0111e40ef63e2dca745c7
Zay-Ro <55798050+Zay-Ro@users.noreply.github.com>
2022-02-05 04:02:12 +0600

    Nigger3

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'classes/ItemList.php';
 
+header('Content-Type: application/json');
 $itemList = new ItemList(['first', 'second', 'third']);
 echo $itemList->getItemsInJson();
-header('application/json');
