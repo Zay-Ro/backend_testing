f78815bf991e4557e1d889f192200177894f8454
Zay-Ro <55798050+Zay-Ro@users.noreply.github.com>
2022-02-05 03:49:45 +0600

    nigger1

---
+++ src/classes/ItemList.php
@@ -0,0 +1,18 @@
+<?php
+class ItemList{
+
+    private array $items;
+
+    public function __construct($items){
+        $this->items = $items;
+    }
+
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    public function getItemsInJson(){
+        return json_encode($this->getItems());
+    }
+}
--- src/index.php
+++ src/index.php
@@ -1,2 +1,5 @@
 <?php
-echo "Nigger";
+require_once 'classes/ItemList.php';
+
+$itemList = new ItemList(['first', 'second', 'third']);
+echo $itemList->getItemsInJson();
